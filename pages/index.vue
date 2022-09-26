57922ec884787d091a09cb5c9d0d8c3dd5daa569
Mosa Nameer <mosa.mn777@gmail.com>
2022-09-26 03:26:23 +0300

    add

--- pages/index.vue
+++ pages/index.vue
@@ -4,17 +4,17 @@
             <div class="text-center">
                 <div class="flex justify-center mb-6">
                     <ColorScheme placeholder="...">
-                    <svg class="w-[20%] self-center" viewBox="0 0 156 189" xmlns="http://www.w3.org/2000/svg">
-                        <path :class="colorMode.value === 'dark' ? 'fill-light' : 'fill-dark'"
-                            d="M76.8455 31.8079L0 0V31.8079L0.000216272 31.8079V63.6157V95.4235V124.874V156.436V156.682L76.8457 189L155.063 156.682V124.874L76.8457 156.682L27.445 136.234V106.783L76.8457 127.231L155.063 95.4235V63.6157L76.8457 95.4235L27.445 74.9756V43.1679L76.8455 63.6157L155.063 31.8079V0L76.8455 31.8079Z"
-                            />
-                    </svg>
+                        <svg class="w-[20%] self-center" viewBox="0 0 156 189" xmlns="http://www.w3.org/2000/svg">
+                            <path :class="colorMode.value === 'dark' ? 'fill-light' : 'fill-dark'"
+                                d="M76.8455 31.8079L0 0V31.8079L0.000216272 31.8079V63.6157V95.4235V124.874V156.436V156.682L76.8457 189L155.063 156.682V124.874L76.8457 156.682L27.445 136.234V106.783L76.8457 127.231L155.063 95.4235V63.6157L76.8457 95.4235L27.445 74.9756V43.1679L76.8455 63.6157L155.063 31.8079V0L76.8455 31.8079Z" />
+                        </svg>
                     </ColorScheme>
                 </div>
                 <h1 class="text-4xl font-bold">نادي النخبة المطورين</h1>
                 <h2 class="text-3xl font-bold">CSIT Developers Club</h2>
             </div>
-            <p class="text-center mx-20">نادي طلابي لتطوير المهارات التقنية وتطوير الحلول التكنولوجية للمجتمع المحلي، الفريق ضمن
+            <p class="text-center mx-20">نادي طلابي لتطوير المهارات التقنية وتطوير الحلول التكنولوجية للمجتمع المحلي،
+                الفريق ضمن
                 مشروع قادة
                 التحوّل الرقمي التابع رسميًا إلى جامعة البصرة، كلية علوم الحاسوب وتكنولوجيا المعلومات.</p>
 
@@ -28,10 +28,14 @@
             </div>
 
             <div v-if="statsPending" class="flex justify-center">
-                <svg class="w-[20%]" version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve">
-                    <path :class="colorMode.value === 'dark' ? 'fill-light' : 'fill-dark'" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
-                    <animateTransform attributeName="transform" attributeType="XML" type="rotate" dur="1s" from="0 50 50" to="360 50 50" repeatCount="indefinite"></animateTransform>
-                </path>
+                <svg class="w-[20%]" version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100"
+                    enable-background="new 0 0 0 0" xml:space="preserve">
+                    <path :class="colorMode.value === 'dark' ? 'fill-light' : 'fill-dark'"
+                        d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
+                        <animateTransform attributeName="transform" attributeType="XML" type="rotate" dur="1s"
+                            from="0 50 50" to="360 50 50" repeatCount="indefinite"></animateTransform>
+                    </path>
                 </svg>
             </div>
             <div v-else class="flex justify-between text-center mx-20">
@@ -41,17 +45,21 @@
                 </div>
             </div>
 
-            <p class="text-3xl font-semibold mb-3 mt-8">أعضاء الفريق</p>
-            
+            <p class="text-3xl md:text-3xl font-semibold mb-3 mt-8 mr-2 md:mr-0">أعضاء الفريق</p>
+
             <div v-if="corePending" class="flex justify-center">
-                <svg class="w-[20%]" version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve">
-                    <path :class="colorMode.value === 'dark' ? 'fill-light' : 'fill-dark'" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
-                    <animateTransform attributeName="transform" attributeType="XML" type="rotate" dur="1s" from="0 50 50" to="360 50 50" repeatCount="indefinite"></animateTransform>
-                </path>
+                <svg class="w-[20%]" version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100"
+                    enable-background="new 0 0 0 0" xml:space="preserve">
+                    <path :class="colorMode.value === 'dark' ? 'fill-light' : 'fill-dark'"
+                        d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
+                        <animateTransform attributeName="transform" attributeType="XML" type="rotate" dur="1s"
+                            from="0 50 50" to="360 50 50" repeatCount="indefinite"></animateTransform>
+                    </path>
                 </svg>
             </div>
 
-            <div v-else class="flex justify-between mt-2">
+            <div v-else class="md:flex justify-between mt-2 mr-2 md:mr-0">
                 <div class="flex flex-col">
                     <div v-for="(member, i) in core[0]?.members" :key="member.id">
                         <div v-if="i % 2 == 0" class="flex mt-5 group">
@@ -60,9 +68,7 @@
                                 <img class="h-16
                                 w-16
                                 border-2
-                                border-dark dark:border-light"
-                                :src="`assets/${member.img}`"
-                                alt="">
+                                border-dark dark:border-light" :src="`assets/${member.img}`" alt="">
                             </div>
                             <div class="mt-1 mr-5">
                                 <p class="text-xl font-semibold">{{member.name}}</p>
@@ -72,7 +78,7 @@
                     </div>
 
                 </div>
-                <div class="m-auto">
+                <div class="m-auto hidden md:block">
                     <svg class="mt-2" width="105" height="127" viewBox="0 0 105 127" fill="none"
                         xmlns="http://www.w3.org/2000/svg">
                         <rect x="3.00267" y="74.5" width="70" height="70" transform="rotate(-45 3.00267 74.5)"
@@ -80,44 +86,45 @@
                         <rect x="3.12132" y="51.6188" width="70" height="70" transform="rotate(-45 3.12132 51.6188)"
                             class="stroke-dark dark:stroke-light" stroke-width="3" />
                         <rect x="14.4309" y="62.9282" width="54.0061" height="54.0061"
-                            transform="rotate(-45 14.4309 62.9282)" fill="#4F009D" class="stroke-dark dark:stroke-light" stroke-width="3" />
+                            transform="rotate(-45 14.4309 62.9282)" fill="#4F009D" class="stroke-dark dark:stroke-light"
+                            stroke-width="3" />
                     </svg>
 
                 </div>
                 <div class="flex flex-col">
-                    <div class="flex flex-col">
-                        <div v-for="(member, i) in core[0]?.members" :key="member.id">
+                    <div v-for="(member, i) in core[0]?.members" :key="member.id">
                         <div v-if="i % 2 != 0" class="flex mt-5 group">
-                            <div class="mt-1 ml-5 text-left">
+                            <div class="mt-1 mr-5 md:mr-0 md:ml-5 md:text-left order-2 md:order-1">
                                 <p class="text-xl font-semibold">{{member.name}}</p>
                                 <p class="">{{member.label}}</p>
                             </div>
                             <div
-                                class="drop-shadow-[-2px_2px_0px_#4F009D] transition ease-in-out group-hover:-translate-y-1 group-hover:drop-shadow-[-4px_4px_0px_#4F009D] duration-200">
+                                class="order-1 md:order-2 drop-shadow-[-2px_2px_0px_#4F009D] transition ease-in-out group-hover:-translate-y-1 group-hover:drop-shadow-[-4px_4px_0px_#4F009D] duration-200">
                                 <img class="h-16
                                 w-16
                                 border-2
-                                border-dark dark:border-light"
-                                :src="`assets/${member.img}`"
-                                alt="">
+                                border-dark dark:border-light" :src="`assets/${member.img}`" alt="">
                             </div>
                         </div>
-                    </div>
                     </div>
                 </div>
             </div>
 
-            <p class="text-3xl font-semibold mb-3 mt-8">آخر المنشورات</p>
+            <p class="text-3xl font-semibold mb-3 mt-10 mr-2 md:mr-0">آخر المنشورات</p>
             <div v-if="postPending" class="flex justify-center">
-                <svg class="w-[20%]" version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve">
-                    <path :class="colorMode.value === 'dark' ? 'fill-light' : 'fill-dark'" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
-                    <animateTransform attributeName="transform" attributeType="XML" type="rotate" dur="1s" from="0 50 50" to="360 50 50" repeatCount="indefinite"></animateTransform>
-                </path>
+                <svg class="w-[20%]" version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100"
+                    enable-background="new 0 0 0 0" xml:space="preserve">
+                    <path :class="colorMode.value === 'dark' ? 'fill-light' : 'fill-dark'"
+                        d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
+                        <animateTransform attributeName="transform" attributeType="XML" type="rotate" dur="1s"
+                            from="0 50 50" to="360 50 50" repeatCount="indefinite"></animateTransform>
+                    </path>
                 </svg>
             </div>
             <div v-else class="flex">
                 <a class="w-full " :href="post._path" v-for="post in posts" :key="post.id">
-                    <div class="bg-light dark:bg-dark
+                    <div class="bg-light dark:bg-dark mx-2 md:mx--0
                     drop-shadow-[2px_2px_0px_#4F009D]
                     flex
                     border-2 border-dark dark:border-light
@@ -125,7 +132,7 @@
                         <div class="m-2 flex flex-col justify-center">
                             <Icon class="w-14 h-14" :name="`fluent-emoji:${post.emoji}`" />
                         </div>
-                        <div class="my-2 flex flex-col justify-center">
+                        <div class="my-2 mt-5 md:mt-0 flex flex-col justify-center">
                             <p class="font-semibold text-lg">{{post.title}}</p>
                             <p>{{post.description}}</p>
                         </div>
@@ -138,13 +145,13 @@
             <div class="flex justify-around text-center">
                 <div class="mt-2 flex flex-col">
                     <span class="self-center">
-                        <Icon name="fluent-emoji:man-teacher-light" class="w-44 h-24" />
+                        <Icon name="fluent-emoji:man-teacher-light" class="w-32 h-16 md:w-44 md:h-24" />
                     </span>
                     <p class="mt-2">عبر تقديم الجلسات في مجال خبرتك.</p>
                 </div>
                 <div class="mt-2 flex flex-col">
                     <span class="self-center">
-                        <Icon name="fluent-emoji:handshake" class="w-44 h-24" />
+                        <Icon name="fluent-emoji:handshake" class="w-32 h-16 md:w-44 md:h-24" />
                     </span>
                     <p class="mt-2">عبر الدعم ورعاية الأحداث للنادي.</p>
                 </div>
